aa151c0f99202f847c9f76df26a44172d185a2a3
CedMrnl <cedric.marinel@eomys.com>
2022-08-12 16:54:24 +0200

    Correct setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import setuptools
-import swat_em
+__title__ = "SWAT-EM - Specific Winding Analyse Tool for electrical machines"
+__version__ = "0.6.3"
+__author__ = "Martin Baun"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2018-2021 Martin Baun"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="swat-em",
-    version=swat_em.__version__,
-    author=swat_em.__author__,
+    version=__version__,
+    author=__author__,
     author_email="mar.baun@googlemail.com",
     description="Specific Winding Analysing Tool for Electrical Machines",
     long_description=long_description,
